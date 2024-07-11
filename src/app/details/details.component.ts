dfbb2b28108da93510dbf8ad2be2fe2745654894
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2024-07-11 13:50:28 +0000

    chore: add routes support, json db, improved api calls, and create housing service

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+    route: ActivatedRoute = inject(ActivatedRoute);
+    housingService = inject(HousingService);
+    housingLocation: HousingLocation | undefined;
+    applyForm = new FormGroup({
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        email: new FormControl(''),    
+    });
+    constructor() {
+        const housingLocationId = Number(this.route.snapshot.params['id'])
+        this.housingService.getHousingLocationById(housingLocationId).then((data) => {
+            this.housingLocation = data;
+        });
+    }
+
+    submitApplication() {
+        this.housingService.submitApplication(
+            this.applyForm.value.firstName ?? '',
+            this.applyForm.value.lastName ?? '',
+            this.applyForm.value.email ?? ''
+        );
+    }
+}
---
+++ src/app/housing.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housing-location';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+
+  url = 'http://localhost:3000/locations';
+
+  constructor() { }
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
+  }
+
+  submitApplication(arg0: string, arg1: string, arg2: string) {
+    console.log(`Application submitted for ${arg0} ${arg1} at ${arg2}`);
+  }
+}
